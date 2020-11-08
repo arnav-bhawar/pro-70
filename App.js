19624c8708faa0929b1cb1421b993415b37e1604
arnav-bhawar <64008302+arnav-bhawar@users.noreply.github.com>
2020-11-08 22:01:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import{createAppContainer} from 'react-navigation';
+import{createBottomTabNavigator} from 'react-navigation-tabs';
+import WriteStoryScreen from './screens/WriteStoryScreen'
+import ReadStoryScreen from './screens/ReadStoryScreen'
+
+
+
+export default class App extends React.Component() {
+  render(){ 
+    return <AppContainer />;
+     
+    }
+  }    
+    <View style={styles.container}>
+     // <Text>Open up App.js to start working on your app!</Text>
+      //<StatusBar style="auto" />
+    </View>
+  
+
+  const TabNavigator = createBottomTabNavigator({
+    Transaction: {screen: ReadStoryScreen},
+    Search: {screen: WriteStoryScreen}
+  },
+  
+  { 
+  defaultNavigationOptions : ({navigation})=>({})
+      tabBarIcon :({})=>{
+
+      }
+    })
+});
+
+  const AppContainer = createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/ReadStoryScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
+
+
+
+
+
+
+
+
+export default class App extends React.Component() {
+    render(){
+        return(
+            <View style = {styles.container}>
+                <Text>Read Story</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
